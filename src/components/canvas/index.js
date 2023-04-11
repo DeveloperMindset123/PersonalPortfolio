ae01db3010a6631e41152f78d83f06c281d71f0f
Ayan Das <109440738+DeveloperMindset123@users.noreply.github.com>
2023-04-11 06:09:08 -0400

    Delete src directory

--- src/components/canvas/index.js
+++
@@ -1,8 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
-
-//this is where we defined all the components shortcut so we can use them as HTML code
